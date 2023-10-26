527a4ef2d64805bdb83083292e6e2eda88b3fe66
Samartha Khare <97874411+SamarthaKhare@users.noreply.github.com>
2023-10-26 15:07:04 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,19 @@ import Hero from './components/Hero'
 import Skills from './components/Skills'
 import WorkEx from './components/Workex';
 import Projects from './components/Projects';
+// wait until window, stylesheets, images, links, and other media assets are loaded
+{
+  document.addEventListener("DOMContentLoaded", function (event) {
+
+    // wait until window, stylesheets, images, links, and other media assets are loaded
+    window.onload = function () {
+
+      document.getElementById("root").style.display = "block";
+
+
+    };
+  });
+}
 
 const App = () => {
   return (
