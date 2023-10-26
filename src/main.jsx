62599d4e18fb90204bd28ad6487ac80c4669c314
Samartha Khare <97874411+SamarthaKhare@users.noreply.github.com>
2023-10-26 14:52:14 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+document.getElementById("root").style.display = "none";
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <App />
 )
+
+
+// wait until window, stylesheets, images, links, and other media assets are loaded
+window.onload = function () {
+  document.getElementById("root").style.display = "block";
+  // Your code and/or method calls here
+
+};
