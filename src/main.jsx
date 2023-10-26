e254bacf215ed314f5090d9b077076563b5267e0
Samartha Khare <97874411+SamarthaKhare@users.noreply.github.com>
2023-10-26 15:06:49 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,20 +4,6 @@ import App from './App.jsx'
 import './index.css'
 
 document.getElementById("root").style.display = "none";
-// wait until window, stylesheets, images, links, and other media assets are loaded
-{
-  document.addEventListener("DOMContentLoaded", function (event) {
-
-    // wait until window, stylesheets, images, links, and other media assets are loaded
-    window.onload = function () {
-
-      document.getElementById("root").style.display = "block";
-
-
-    };
-  });
-}
-
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <App />
